df959916e364fff8358fe4ecfa728cecadc38f80
Veeramanikandan <vmksince2002@gmail.com>
2025-08-22 13:02:06 +0530

    using jframe created login page

---
+++ JFrame_Project/src/main/java/com/mycompany/mainflowinternproject/MainflowInternProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mainflowinternproject;
+
+/**
+ *
+ * @author Veeramanikandan
+ */
+public class MainflowInternProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
